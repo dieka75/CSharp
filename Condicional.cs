82193c5e1174e94291230b03c7f87d3a8e93ea29
Diego Katz <diekatz@gmail.com>
2023-04-05 20:33:42 -0300

    se agrego condicionales

---
+++ Condicional.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("que edad tenes?");
+
+            int edad = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("tenes carnet de conducir");
+
+            carnet = ReadLine();
+
+        }
+    }
+}
